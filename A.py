dc85e8f721d7fc69ff0de2260601140a2f759a83
José Manuel Cortes Cerón <145143953+Joseecodm@users.noreply.github.com>
2024-09-30 18:57:56 -0600

    A

---
+++ A.py
@@ -0,0 +1,10 @@
+num1 = int(input("Puntaje de Espino: "))
+num2 = int(input("Puntaje de Karel: "))
+
+if num1 > num2: 
+    print("Espino")
+elif num1 == num2:
+    print("Empate")
+else:
+    print("Karel")
+
